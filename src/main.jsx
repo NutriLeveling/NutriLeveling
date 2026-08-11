42aa2daa21da58c26921bb0bd712eae019995a8e
Loizos Georgiou <nutrileveling@gmail.com>
2026-08-11 12:54:18 +0200

    Self-host fonts and update dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,15 @@ import "@fontsource/space-grotesk/600.css";
 import "@fontsource/space-grotesk/700.css";
 import "@fontsource/vt323";
 import "@fontsource/press-start-2p";
+import "@fontsource/orbitron/500.css";
+import "@fontsource/orbitron/600.css";
+import "@fontsource/orbitron/700.css";
+import "@fontsource/orbitron/800.css";
+
+import "@fontsource/sora/400.css";
+import "@fontsource/sora/600.css";
+import "@fontsource/sora/700.css";
+import "@fontsource/sora/800.css";
 
 import App from "./App";
 import "./index.css";
